dd52531af084a50c3b40c35869845912d126dd80
이주영 <juu0124@wemakeprice.com>
2020-08-24 10:40:58 +0900

    (master): 초기화

--- my-lerna-test/packages/utils/lib/utils.js
+++ my-lerna-test/packages/utils/lib/utils.js
@@ -4,24 +4,5 @@ module.exports = utils;
 
 function utils() {
     // TODO
-    // feature02 테스트
-}
-
-function test2() {
-  // feature02 테스트
-
-
-    // master test
-}
-
-function masterTest() {
-  // master test
-
-    // feature01 테스트
-
-}
-
-function test() {
-  // feature01 테스트
 
 }
